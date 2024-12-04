d3aa0663328a3390ebafc9b706f8698924345dbd
NushK650 <Nush8653@gmail.com>
2024-12-04 14:45:27 -0800

    just to save

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,55 @@
-// Get all time buttons
 const timeButtons = document.querySelectorAll('.time-btn');
+const dailyBtn = document.getElementById('dailyBtn');
+const weeklyBtn = document.getElementById('weeklyBtn');
+const monthlyBtn = document.getElementById('monthlyBtn');
+let workHours = document.getElementById('workHours');
+let workPrevious = document.getElementById('workPrevious');
 
-// Add click event listener to each button
+
+
+
+function getData(){
+    return fetch('../data.json')
+    .then( response => response.json())
+    .then( data => {
+        console.log(data)
+        return data
+    })   
+}
+getData();
+
+
+
 timeButtons.forEach(button => {
     button.addEventListener('click', () => {
-        // Remove active class from all buttons
+        
         timeButtons.forEach(btn => btn.classList.remove('active'));
         
-        // Add active class to clicked button
+        
         button.classList.add('active');
     });
+});
+
+dailyBtn.addEventListener('click', function(){
+    
+    
+    getData();
+    const workDailyHours = Data[0].timeframes.daily.current;
+    const workDailyPrevious = Data[0].timeframes.daily.previous;
+        console.log(workDailyHours);  
+        console.log(workDailyPrevious);  
+        
+        
+        
+   
+     
+        
+    
+});
+weeklyBtn.addEventListener('click',function(){
+
+});
+
+monthlyBtn.addEventListener('click', function(){
+
 });
